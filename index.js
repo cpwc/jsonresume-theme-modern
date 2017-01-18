078e9c06e75d356f42ef5af1ac7e9c195b2b010a
Wei Cheng <calvinpohwc@gmail.com>
2017-01-18 12:45:18 +0800

    Added month to work experience.

--- index.js
+++ index.js
@@ -5,39 +5,115 @@ var Mustache = require('mustache');
 
 function render(resumeObject) {
 
-
-	_.each(resumeObject.work, function(w){
+	_.each(resumeObject.work, function(w) {
 		w.startDateYear = w.startDate.substr(0,4);
+		switch (w.startDate.substr(5,2)) {
+			case '01':
+				w.startDateMonth = "Jan";
+				break;
+			case '02':
+				w.startDateMonth = "Feb";
+				break;
+			case '03':
+				w.startDateMonth = "Mar";
+				break;
+			case '04':
+				w.startDateMonth = "Apr";
+				break;
+			case '05':
+				w.startDateMonth = "May";
+				break;
+			case '06':
+				w.startDateMonth = "Jun";
+				break;
+			case '07':
+				w.startDateMonth = "Jul";
+				break;
+			case '08':
+				w.startDateMonth = "Aug";
+				break;
+			case '09':
+				w.startDateMonth = "Sep";
+				break;
+			case '10': 
+				w.startDateMonth = "Oct";
+				break;
+			case '11':
+				w.startDateMonth = "Nov";
+				break;
+			case '12':
+				w.startDateMonth = "Dec";
+				break;
+		}
+
 		if(w.endDate) {
 			w.endDateYear = w.endDate.substr(0,4);
+			switch ((w.endDate || "").substr(5,2)) {
+				case '01':
+					w.startDateMonth = "Jan";
+					break;
+				case '02':
+					w.startDateMonth = "Feb";
+					break;
+				case '03':
+					w.startDateMonth = "Mar";
+					break;
+				case '04':
+					w.startDateMonth = "Apr";
+					break;
+				case '05':
+					w.startDateMonth = "May";
+					break;
+				case '06':
+					w.startDateMonth = "Jun";
+					break;
+				case '07':
+					w.startDateMonth = "Jul";
+					break;
+				case '08':
+					w.startDateMonth = "Aug";
+					break;
+				case '09':
+					w.startDateMonth = "Sep";
+					break;
+				case '10': 
+					w.startDateMonth = "Oct";
+					break;
+				case '11':
+					w.startDateMonth = "Nov";
+					break;
+				case '12':
+					w.startDateMonth = "Dec";
+					break;
+			}
 		} else { 
 			w.endDateYear = 'Present'
 		}
 	});
-	_.each(resumeObject.education, function(e){
-    if( !e.area || !e.studyType ){
-      e.educationDetail = (e.area == null ? '' : e.area) + (e.studyType == null ? '' : e.studyType);
-    }  else {
-      e.educationDetail = e.area + ", "+ e.studyType;
-    }
+	_.each(resumeObject.education, function(e) {
+		if( !e.area || !e.studyType ){
+			e.educationDetail = (e.area == null ? '' : e.area) + (e.studyType == null ? '' : e.studyType);
+		} else {
+			e.educationDetail = e.area + ", "+ e.studyType;
+		}
 		e.startDateYear = e.startDate.substr(0,4);
-		if(e.endDate) {
+		if (e.endDate) {
 			e.endDateYear = e.endDate.substr(0,4);
-		}  else { 
+		} else { 
 			e.endDateYear = 'Present'
 		}
 	});
 	if(resumeObject.basics && resumeObject.basics.email) {
 		resumeObject.basics.gravatar = gravatar.url(resumeObject.basics.email, {
-                        s: '100',
-                        r: 'pg',
-                        d: 'mm'
-                    });
+			s: '100',
+			r: 'pg',
+			d: 'mm'
+		});
 	}
 	resumeObject.profiles = {};
 
 	_.each(resumeObject.basics.profiles, function(profile){
-    	resumeObject.profiles[profile.network] = profile.username;
+		resumeObject.profiles[profile.network] = profile.username;
 	});
 	console.log(resumeObject.profiles);
 	var theme = fs.readFileSync(__dirname + '/resume.template', 'utf8');
